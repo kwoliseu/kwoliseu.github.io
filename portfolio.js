26b7fbf6ea33c5eb88fe93426e4034cebb4fc4f1
kwoliseu <44662437+kwoliseu@users.noreply.github.com>
2018-11-15 09:28:26 +0900

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,55 +1,35 @@
-
+$(document).ready(function(){
+    $(".allBtn p").click(function(){
+        $(".topWrap li p").removeClass("on")
+        $(this).addClass("on")
         
-        $(document).ready(function(){
-            $(".con1Wrap .btnBox li").click(function(){
-                
-                var eqBtn = $(this).index();
-                var eqLst = $(".con1Wrap .listBox div").index();
-                
-                $(this).addClass("on");
-                $(this).siblings().removeClass("on");
-                $(".con1Wrap .listBox > div").eq(eqBtn).addClass("on");
-                $(".con1Wrap .listBox  > div").eq(eqBtn).siblings().removeClass("on");
-                
-            });
-            $(".con2Wrap .btnBox li").click(function(){
-                
-                var eqBtn = $(this).index();
-                var eqLst = $(".con2Wrap .listBox div").index();
-                
-                $(this).addClass("on");
-                $(this).siblings().removeClass("on");
-                $(".con2Wrap .listBox > div").eq(eqBtn).addClass("on");
-                $(".con2Wrap .listBox  > div").eq(eqBtn).siblings().removeClass("on");
-                 
-            });
-            $(".con3Wrap .btnBox li").click(function(){
-                
-                var eqBtn = $(this).index();
-                var eqLst = $(".con3Wrap .listBox div").index();
-                
-                $(this).addClass("on");
-                $(this).siblings().removeClass("on");
-                $(".con3Wrap .listBox > div").eq(eqBtn).addClass("on");
-                $(".con3Wrap .listBox  > div").eq(eqBtn).siblings().removeClass("on");
-                
-            });
-        });
-        
-        
-        
-        $(document).ready(function () {
-        	var sc_pg = parseInt($(window).height());
-			$(window).on('mousewheel', function(e){
-				if(e.originalEvent.wheelDelta < 0) {
-					$('html, body').not(":animated").animate({
-                        scrollTop : '+='+sc_pg+'px'},800);
-				} else {
-					$('html, body').not(":animated").animate({
-                        scrollTop : '-='+sc_pg+'px'},800);
-				}
-				return false;
-			});
-		});
-        
+        $(".cardList div").removeClass("off")
+    });
     
+    
+    $(".dtBtn p").click(function(){
+        $(".topWrap li p").removeClass("on")
+        $(this).addClass("on")
+        
+        $(".cardList div").removeClass("off")
+        $(".cardList .mb, .cardList .rs").addClass("off")
+    });
+    
+    
+    $(".mbBtn p").click(function(){
+        $(".topWrap li p").removeClass("on")
+        $(this).addClass("on")
+        
+        $(".cardList div").removeClass("off")
+        $(".cardList .rs, .cardList .dt").addClass("off")
+    });
+    
+    
+    $(".rsBtn p").click(function(){
+        $(".topWrap li p").removeClass("on")
+        $(this).addClass("on")
+        
+        $(".cardList div").removeClass("off")
+        $(".cardList .mb, .cardList .dt").addClass("off")
+    });
+});
