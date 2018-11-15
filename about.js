48b65e052d738513e56ce808950faced4bbb7b77
kwoliseu <44662437+kwoliseu@users.noreply.github.com>
2018-11-15 12:10:35 +0900

    Update about.js

--- about.js
+++ about.js
@@ -4,3 +4,21 @@
             $(".logo2").fadeIn(1000);
             $(".my, .text").delay(500).fadeIn(800);
         });
+
+
+
+        
+        $(document).ready(function () {
+        	var sc_pg = parseInt($(window).height());
+			$(window).on('mousewheel', function(e){
+				if(e.originalEvent.wheelDelta < 0) {
+					$('html, body').not(":animated").animate({
+                        scrollTop : '+='+sc_pg+'px'},800);
+				} else {
+					$('html, body').not(":animated").animate({
+                        scrollTop : '-='+sc_pg+'px'},800);
+				}
+				return false;
+			});
+		});
+        
